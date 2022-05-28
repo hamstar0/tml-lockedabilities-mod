da4802645acb5aad0f3db64bdb00314a91cfa546
hamstar0 <webutek@gmail.com>
2022-05-27 19:37:48 -0700

    v1.3.2 - Added (incomplete) API

---
+++ LockedAbilities/API.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+
+
+namespace LockedAbilities {
+	public class LockedAbilitiesAPI {
+		public static int GetAllowedAccessorySlots( Player player ) {
+			var myplayer = player.GetModPlayer<LockedAbilitiesPlayer>();
+
+			return myplayer.InternalAllowedAccessorySlots;
+		}
+
+		public static void SetAllowedAccessorySlots( Player player, int slots ) {
+			var myplayer = player.GetModPlayer<LockedAbilitiesPlayer>();
+
+			myplayer.SetAllowedAccessorySlots( slots );
+		}
+	}
+}
